9207abcdcd2649d09cd642d034406680455434ce
gscmariokart <gscmariokart@gmail.com>
2026-09-12 18:15:45 -0500

    Delete dx/base.py

--- dx/base.py
+++
@@ -1,9 +0,0 @@
-from discord import app_commands
-from discord.ext import commands
-
-
-@app_commands.guild_only()
-class Base(
-    commands.GroupCog, name="dx", description="Mario Kart 8 Deluxe timetrial commands"
-):
-    pass
